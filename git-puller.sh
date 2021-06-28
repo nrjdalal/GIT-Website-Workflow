9faaa551b22c13a7f0213ca090ef7538e0becf9a
Neeraj Dalal <nd941z@gmail.com>
2021-06-28 18:18:33 +0530

    Update git-puller.sh

--- git-puller.sh
+++ git-puller.sh
@@ -1,18 +1,3 @@
-echo
-echo "----------------------------"
-echo " Creator - Neeraj Dalal     "
-echo " Email - admin@nrjdalal.com "
-echo "----------------------------"
-
-echo
-echo "A simple script to set up an efficient development workflow using Git to manage a live website"
-echo "For more information visit - https://nrjdalal.com/GIT-Website-Workflow"
-
-echo
-echo "Prerequisite - Git must be installed on both local and remote machine/ web server"
-echo "To download Git visit - https://git-scm.com/downloads"
-
-echo
 read -p "SSH Username > " USERNAME
 read -p "SSH Hostname > " HOSTNAME
 read -p "SSH Port > " PORT
@@ -28,6 +13,5 @@ git pull $MASTER
 exit
 GIT
 
-echo
 ssh -p $PORT $USERNAME@$HOSTNAME "bash -s" <server.sh
 rm server.sh
